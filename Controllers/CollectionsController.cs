5f3466ee6ca7490cdc10736c4664437b19365ce9
Dmitry Gridnev <dmitriy.gridnev@outlook.com>
2022-05-15 21:59:00 +0300

    Third commit

--- Controllers/CollectionsController.cs
+++ Controllers/CollectionsController.cs
@@ -16,7 +16,7 @@ namespace Taxii.Controllers
         [HttpGet()]
         public string Get()
         {
-            var res = "{\"collections\": [{\"can_read\": true,\"can_write\": false,\"description\": \"\",\"id\": \"107\",\"title\": \"Phish Tank\"},{\"can_read\": true,\"can_write\": false,\"description\": \"\",\"id\": \"67394485-b4dc-4628-b3a2-079000fee115\",\"title\": \"Abuse.ch Ransomware IPs\"}]}";
+            var res = "{\"collections\": [{\"can_read\": true,\"can_write\": false,\"description\": \"\",\"id\": \"110\",\"title\": \"Phish Tank\"},{\"can_read\": true,\"can_write\": false,\"description\": \"\",\"id\": \"67394485-b4dc-4628-b3a2-079000fee115\",\"title\": \"Abuse.ch Ransomware IPs\"}]}";
 
         return res;
         }
@@ -26,8 +26,8 @@ namespace Taxii.Controllers
         public string GetCollection([FromRoute] string id)
         {
             switch (id) {
-                case "107":
-                    return "{\"can_read\": true, \"can_write\": false, \"description\": \"\", \"id\": \"107\", \"title\": \"Phish Tank\"}";
+                case "110":
+                    return "{\"can_read\": true, \"can_write\": false, \"description\": \"\", \"id\": \"110\", \"title\": \"Phish Tank\"}";
                 case "67394485-b4dc-4628-b3a2-079000fee115":
                     return "{\"can_read\": true,\"can_write\": false,\"description\": \"\",\"id\": \"67394485-b4dc-4628-b3a2-079000fee115\",\"title\": \"Abuse.ch Ransomware IPs\"}";
                 default: return "";
@@ -40,7 +40,7 @@ namespace Taxii.Controllers
         {
             switch (id)
             {
-                case "107":
+                case "110":
                     return "{\"id\": \"bundle--70ec1e8c-733e-418b-bbe1-1e5dd33c9f43\",\"objects\": [{\"created\": \"2016-02-26T18:11:48.479Z\",\"description\": \"TS ID: 37323558; iType: phish_url; State: active; Org: Web Werks; Source: Phish Tank\",\"id\": \"indicator--5aabc09b-6c50-4259-98b2-c2577f446a31\",\"labels\": [\"malicious-activity\",\"threatstream-severity-very-high\",\"threatstream-confidence-85\"],\"modified\": \"2016-02-26T18:11:48.479Z\",\"name\": \"phish_url: http://www.srbiohealth.com/London1/Eruku/nD/index.php\",\"object_marking_refs\": [\"marking-definition--34098fce-860f-48ae-8e50-ebd3cc5e41da\"],\"pattern\": \"[url:value = 'http://www.srbiohealth.com/London1/Eruku/nD/index.php']\",\"type\": \"indicator\",\"valid_from\": \"2016-02-26T18:11:48.479Z\"},{\"created\": \"2016-02-26T18:11:49.452Z\",\"description\": \"TS ID: 37323554; iType: phish_url; State: active; Org: CyrusOne LLC; Source: Phish Tank\",\"id\": \"indicator--0da61560-61b1-473d-a685-28bebfb4c5ab\",\"labels\": [\"malicious-activity\",\"threatstream-severity-very-high\",\"threatstream-confidence-85\"],\"modified\": \"2016-02-26T18:11:49.452Z\",\"name\": \"phish_url: http://dayamino.com/New/\",\"object_marking_refs\": [\"marking-definition--34098fce-860f-48ae-8e50-ebd3cc5e41da\"],\"pattern\": \"[url:value = 'http://dayamino.com/New/']\",\"type\": \"indicator\",\"valid_from\": \"2016-02-26T18:11:49.452Z\"}],\"spec_version\": \"2.0\",\"type\": \"bundle\"}";
                 case "67394485-b4dc-4628-b3a2-079000fee115":
                     return "{\"id\": \"bundle--70ec1e8c-733e-418b-bbe1-1e5dd33c9f43\",\"objects\": [{\"created\": \"2016-02-26T18:11:48.479Z\",\"description\": \"TS ID: 37323558; iType: phish_url; State: active; Org: Web Werks; Source: Phish Tank\",\"id\": \"indicator--5aabc09b-6c50-4259-98b2-c2577f446a31\",\"labels\": [\"malicious-activity\",\"threatstream-severity-very-high\",\"threatstream-confidence-85\"],\"modified\": \"2016-02-26T18:11:48.479Z\",\"name\": \"phish_url: http://www.srbiohealth.com/London1/Eruku/nD/index.php\",\"object_marking_refs\": [\"marking-definition--34098fce-860f-48ae-8e50-ebd3cc5e41da\"],\"pattern\": \"[url:value = 'http://www.srbiohealth.com/London1/Eruku/nD/index.php']\",\"type\": \"indicator\",\"valid_from\": \"2016-02-26T18:11:48.479Z\"},{\"created\": \"2016-02-26T18:11:49.452Z\",\"description\": \"TS ID: 37323554; iType: phish_url; State: active; Org: CyrusOne LLC; Source: Phish Tank\",\"id\": \"indicator--0da61560-61b1-473d-a685-28bebfb4c5ab\",\"labels\": [\"malicious-activity\",\"threatstream-severity-very-high\",\"threatstream-confidence-85\"],\"modified\": \"2016-02-26T18:11:49.452Z\",\"name\": \"phish_url: http://dayamino.com/New/\",\"object_marking_refs\": [\"marking-definition--34098fce-860f-48ae-8e50-ebd3cc5e41da\"],\"pattern\": \"[url:value = 'http://dayamino.com/New/']\",\"type\": \"indicator\",\"valid_from\": \"2016-02-26T18:11:49.452Z\"}],\"spec_version\": \"2.0\",\"type\": \"bundle\"}";
